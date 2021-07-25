0ad3ea40d1687e98aaf4192e40a0f9cd42e4fb45
jvnko <jvnko2134@yahoo.com>
2021-07-24 20:52:26 -0600

    Adding double window buffer to increase """""smoothness""""" during gameplay and remove "snapping" later on
    Added non-frame-dependant timing; based on deltaTime instead off of frame Times

--- src/DEFINITIONS.java
+++ src/DEFINITIONS.java
@@ -2,6 +2,6 @@ public class DEFINITIONS {
 
     public static final int SCREEN_WIDTH = 1280;
     public static final int SCREEN_HEIGHT = 720;
-    public static final String WINDOW_TITLE = "Snake";
+    public static final String WINDOW_TITLE = "Blake the Snake";
 
 }
--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,9 @@
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class Window extends JFrame implements Runnable {
+
+    public boolean isRunning;
 
     public Window(int width, int height, String title) {
 
@@ -10,10 +13,49 @@ public class Window extends JFrame implements Runnable {
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        isRunning = true;
+
+    }
+
+    public void update(double dt)    {
+
+        Image dbImage = createImage(getWidth(),getHeight());
+
+        Graphics dbg = dbImage.getGraphics();
+
+        this.draw(dbg);
+
+        getGraphics().drawImage(dbImage,0,0,this);
+
+    }
+
+    public void draw(Graphics g)  {
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.BLACK);
+
+        g2.fillRect(0,0,getWidth(),getHeight());
+
     }
 
     @Override
     public void run() {
+
+        double lastFrameTime = 0.0f;
+
+        try {
+
+            while(isRunning) {
+
+                double time = Time.getTime();
+                double deltaTime = time - lastFrameTime;
+                lastFrameTime = time;
+                update(deltaTime);
+
+            }
+
+        } catch(Exception e){e.printStackTrace();}
 
     }
 }
