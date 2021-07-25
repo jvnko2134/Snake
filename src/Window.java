edd1acdfece647adcfba24ac976e1278d63e870f
jvnko <jvnko2134@yahoo.com>
2021-07-24 20:55:58 -0600

    Adding double window buffer to increase """""smoothness""""" during gameplay and remove "snapping" later on
    Added non-frame-dependant timing; based on deltaTime instead off of frame Times

--- src/Window.java
+++ src/Window.java
@@ -34,7 +34,6 @@ public class Window extends JFrame implements Runnable {
         Graphics2D g2 = (Graphics2D)g;
 
         g2.setColor(Color.BLACK);
-
         g2.fillRect(0,0,getWidth(),getHeight());
 
     }
