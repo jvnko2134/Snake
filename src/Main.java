6e43f572b50b9b9acb2b636efbe40064a8a3f6c1
jvnko <jvnko2134@yahoo.com>
2021-07-24 20:43:21 -0600

    Adding Window properties

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+
+
 public class Main {
 
     public static void main(String[] args)   {
 
-
+        Window window = new Window(DEFINITIONS.SCREEN_WIDTH, DEFINITIONS.SCREEN_HEIGHT, DEFINITIONS.WINDOW_TITLE);
 
     }
 
--- src/Window.java
+++ src/Window.java
@@ -1,5 +1,19 @@
-public class Window {
+import javax.swing.JFrame;
 
+public class Window extends JFrame implements Runnable {
 
+    public Window(int width, int height, String title) {
 
+        setSize(width,height);
+        setResizable(false);
+        setTitle(title);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
