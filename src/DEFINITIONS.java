43e58c1800f5314d5db1bbd30b4b0d3369b27978
jvnko <jvnko2134@yahoo.com>
2021-07-24 21:46:43 -0600

    god i fucking hate this ide

--- src/DEFINITIONS.java
+++
@@ -1,7 +0,0 @@
-public class DEFINITIONS {
-
-    public static final int SCREEN_WIDTH = 1280;
-    public static final int SCREEN_HEIGHT = 720;
-    public static final String WINDOW_TITLE = "Blake the Snake";
-
-}
--- src/Scene.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Graphics;
-
-public abstract class Scene {
-
-    public abstract void update(double dt);
-
-    public abstract void draw(Graphics g);
-
-}
